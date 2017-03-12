fe5479a3538f31e8531a7c1e0b5f96410a62a9cc
Daniel <bullsbbqtn@gmail.com>
2017-03-12 16:43:30 -0400

    login form compleated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
+import firebase from 'firebase'
 import {
 	EMAIL_CHANGED, 
-	PASSWORD_CHANGED
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL,
+	LOGIN_USER
 } from './types'
 
 export const emailChanged = (text) => {
@@ -16,3 +20,42 @@ export const passwordChanged = (text) => {
 		payload: text
 	};
 };
+
+export const loginUser = ({email, password}) => {
+	return (dispatch) => {
+		dispatch({type: LOGIN_USER})
+		
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(user => loginUserSuccess(dispatch, user))
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(user => loginUserSuccess(dispatch, user))
+					.catch(() => loginUserFail(dispatch));
+		});
+	};
+};
+const loginUserFail = (dispatch) => {
+	dispatch({type: LOGIN_USER_FAIL})
+}
+
+const loginUserSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
